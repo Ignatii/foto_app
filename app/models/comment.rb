0b02ae3aa2c49d2933db336ee5dbf36de91fe71c
Ignatii <ignatiy.anatolich@mail.ru>
2018-03-21 14:37:47 +0300

    comment length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 # model comments for comments in app
 class Comment < ApplicationRecord
   validates :user_id, presence: true
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable, dependent: :destroy
 end
