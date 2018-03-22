7a985119411ed0d21dc823d156def7fb36022251
Ignatii <ignatiy.anatolich@mail.ru>
2018-03-22 17:51:27 +0300

    modified

--- db/migrate/20180322094649_add_insta_token_to_users.rb
+++ db/migrate/20180322094649_add_insta_token_to_users.rb
@@ -1,5 +1,5 @@
 class AddInstaTokenToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :insta_token, :string
+    add_column :users, :insta_token, :string, default: ''
   end
 end
